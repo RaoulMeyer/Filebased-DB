d1ef2d7b7047b77f8137e7f99876c0cd4684fd1c
RaoulMeyer <raouljmeyer@gmail.com>
2016-01-19 07:20:05 +0100

    Added most used method shortcuts to Database class

--- core/Database.php
+++ core/Database.php
@@ -18,7 +18,7 @@ class Database {
      *
      * @return Collection
      */
-    public static function get($entity) {
+    public static function getCollection($entity) {
         if(!$entity instanceof Entity) {
             return null;
         }
@@ -28,5 +28,56 @@ class Database {
         }
 
         return Database::$collections[get_class($entity)];
+    }
+
+    /**
+     * Get all data from a Collection
+     *
+     * @param Entity $entity Entity for which to get data
+     *
+     * @return array Array of all Entities found
+     */
+    public static function getAll($entity) {
+        $collection = self::getCollection($entity);
+
+        return $collection->get();
+    }
+
+    /**
+     * Get all data from a Collection with filters
+     *
+     * @param Entity $entity Entity for which to get data
+     * @param array $filters Array of field => value pairs to filter by
+     *
+     * @return array Array of all Entities found
+     */
+    public static function get($entity, $filters = array()) {
+        $collection = self::getCollection($entity);
+
+        foreach($filters as $field => $value) {
+            $collection->filter($field, $value);
+        }
+
+        return $collection->get();
+    }
+
+    /**
+     * Save an Entity
+     *
+     * @param Entity $entity Entity to save
+     */
+    public static function save($entity) {
+        $collection = self::getCollection($entity);
+        $collection->save($entity);
+    }
+
+    /**
+     * Remove an Entity
+     *
+     * @param Entity $entity Entity to remove
+     */
+    public static function remove($entity) {
+        $collection = self::getCollection($entity);
+        $collection->remove($entity);
     }
 }
